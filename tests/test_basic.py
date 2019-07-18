32d623c6b014229a8960c60b2df42aac757676ae
klemen_git <zaletelj.klemen@gmail.com>
2019-07-18 15:37:14 +0200

    Documentation and a test.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,11 @@
 
 """ Code unit-tests go here. """
+import numpy as np
 
 import image_ema_project
+
+def test_roi_xy():
+    xy = np.arange(-4, 5)
+    result = [xy, xy]
+    roi_xy = image_ema_project.roi_xy([0, 0], [8, 8])
+    assert np.all(np.equal(roi_xy, result))
